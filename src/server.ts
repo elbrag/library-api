f5903bd35682050f58f74c5f42eb21d4e4eec2e4
Ellen <ellen.brage@welcom.se>
2025-06-06 14:12:53 +0200

    Remove unnecessary endpoint and function

--- src/server.ts
+++ src/server.ts
@@ -5,11 +5,3 @@ import app from "./app";
 app.listen(PORT, () => {
 	console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-app.get("/status", (_request: any, response: any) => {
-	const status = {
-		Status: "I'm up!",
-	};
-
-	response.send(status);
-});
