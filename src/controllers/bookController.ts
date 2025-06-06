a038c6bc383ef46905b5c9e34387c52059e18ba3
Ellen <ellen.brage@welcom.se>
2025-06-06 14:54:25 +0200

    Update controller with the rest of the functions, wip

--- src/controllers/bookController.ts
+++ src/controllers/bookController.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import { Book, books } from "../models/book";
-import { books as booksData } from "../db/books";
+import { books as bookList } from "../db/books";
 
 /**
  * Get books
@@ -11,7 +11,7 @@ export const getBooks = (
 	next: NextFunction
 ) => {
 	try {
-		response.json(booksData);
+		response.json(bookList);
 	} catch (error) {
 		next(error);
 	}
@@ -24,7 +24,15 @@ export const addBook = (
 	request: Request,
 	response: Response,
 	next: NextFunction
-) => {};
+) => {
+	try {
+		const book = request.body;
+		bookList.push(book);
+		response.status(201).json(bookList); // In reality we would of course make a new GET call rather than expect a result here for the updated list. Then the return value here would simply be an ok status, not the new book list
+	} catch (error) {
+		next(error);
+	}
+};
 
 /**
  * Delete book
@@ -33,7 +41,16 @@ export const deleteBook = (
 	request: Request,
 	response: Response,
 	next: NextFunction
-) => {};
+) => {
+	try {
+		const bookId = parseInt(request.params.id);
+		if (!bookId) throw new Error("This book is not in the library");
+		const newBookList = bookList.filter((book) => book.id !== bookId);
+		response.status(200).json(newBookList);
+	} catch (error) {
+		next(error);
+	}
+};
 
 /**
  * Update book
@@ -42,4 +59,16 @@ export const updateBook = (
 	request: Request,
 	response: Response,
 	next: NextFunction
-) => {};
+) => {
+	try {
+		const bookId = parseInt(request.params.id);
+		const bookIndex = bookList.findIndex((book) => book.id === bookId);
+		if (!bookIndex) throw new Error("This book is not in the library");
+
+		const newData = request.body;
+		bookList[bookIndex] = { ...bookList[bookIndex], ...newData };
+		response.status(200).json(bookList);
+	} catch (error) {
+		next(error);
+	}
+};
