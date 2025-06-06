5332ea912645428cc0778fb7c81e4bac07a7e74f
Ellen <ellen.brage@welcom.se>
2025-06-06 11:55:50 +0200

    Install and config typescript and nodemon, restructure project a bit

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-const express = require("express");
+import express, { Request, Response } from "express";
+
 const app = express();
 app.use(express.json());
 const PORT = 4000;
@@ -7,9 +8,9 @@ app.listen(PORT, () => {
 	console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-app.get("/status", (request, response) => {
+app.get("/status", (request: Request, response: Response) => {
 	const status = {
-		Status: "I'm up",
+		Status: "I'm up!",
 	};
 
 	response.send(status);
